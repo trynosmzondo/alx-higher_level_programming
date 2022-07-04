50fadcf7d98290ad6029475331066c3b54dffa51
trynosmzondo <trynosm68@gmail.com>
2022-07-04 07:17:15 -0700

    My commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
-#!/usr/bin.py
-def print_list_integer(my_list=[]):
-    for item in my_list:
-        print('{:d}'.format(item))
-
-
-if __name__ == "__main__":
-    print_list_integer(my_list)
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for elm in my_list:
+        print("{:d}".format(elm))
