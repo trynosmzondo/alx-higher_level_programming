1fcc519ffebde6bd6fa95478059ccd7ade2ed8dd
trynosmzondo <trynosm68@gmail.com>
2022-07-04 07:35:50 -0700

    My commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
-        return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
-
-
-if __name__ == "__main__":
-    return max_integer(my_list)
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for elm in my_list:
+            if elm > max:
+                max = elm
+        return max
+    return None
